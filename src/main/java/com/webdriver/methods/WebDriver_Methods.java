988f857345fc875b39029f798d5f5a298b0bf7b6
akshaykumargoase <akshaykumargodase@gmail.com>
2022-12-23 15:30:51 +0530

    Deleted unused packages;

--- src/main/java/com/webdriver/methods/WebDriver_Methods.java
+++ src/main/java/com/webdriver/methods/WebDriver_Methods.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
